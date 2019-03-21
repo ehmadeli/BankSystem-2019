73c7e5d77173b08a94e86b44770f6d17861ad961
shul3 <ssulziks@gmail.com>
2019-03-21 14:13:57 +0100

    Fixa bug

--- www/js/Betalning.js
+++ www/js/Betalning.js
@@ -29,7 +29,7 @@ class Betalning {
     let account = App.user.accounts.filter(account => account.accountNumber === f.accountNumber)[0];
     let accountTo = App.user.accounts.filter(account => account.accountNumber === f.accountNumberTo)[0];
     // Deposit or withdraw from different accounts
-    if((account != accountTo) && (account.checkBalance(f.sum)){
+    if((account != accountTo) && (account.checkBalance(f.sum))){
       account.withdraw(f.label + ' to: ' + f.accountNumberTo, f.sum);
       accountTo.deposit(f.label + ' from: ' + f.accountNumber, f.sum);
     } else {
